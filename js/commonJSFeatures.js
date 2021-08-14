25402fb273ad3b60eae75ba8a66939a7707efd42
CiaoAnkit <8746855+CiaoAnkit@users.noreply.github.com>
2021-08-15 00:30:58 +0530

    MinorFixes

--- js/commonJSFeatures.js
+++ js/commonJSFeatures.js
@@ -79,7 +79,7 @@ document.getElementById("header_area_id").innerHTML +=  "\
 	var curr_year = new Date();
 
 	if (footer_id == 1) {
-		update_date = "August 01, 2021";
+		update_date = "August 15, 2021";
 	}
 
 	if (footer_id == 2) {
