3789b8f7705260f1e9e12d5056649bad82e43ab7
AnkitYanis <8746855+CiaoAnkit@users.noreply.github.com>
2022-05-06 15:09:41 +0530

    RA_Dev_MultiplePositions

--- js/commonJSFeatures.js
+++ js/commonJSFeatures.js
@@ -79,7 +79,7 @@ document.getElementById("header_area_id").innerHTML +=  "\
 	var curr_year = new Date();
 
 	if (footer_id == 1) {
-		update_date = "April 18, 2022";
+		update_date = "May 05, 2022";
 	}
 
 	if (footer_id == 2) {
