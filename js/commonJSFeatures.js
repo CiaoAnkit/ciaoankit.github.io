855ef0fa5f522c4915bf0bd946942c9fb12fc7eb
AnkitYanis <8746855+CiaoAnkit@users.noreply.github.com>
2022-12-01 15:24:29 +0530

    MinorFixes_BibFixes

--- js/commonJSFeatures.js
+++ js/commonJSFeatures.js
@@ -79,7 +79,7 @@ document.getElementById("header_area_id").innerHTML +=  "\
 	var curr_year = new Date();
 
 	if (footer_id == 1) {
-		update_date = "October 28, 2022";
+		update_date = "November 21, 2022";
 	}
 
 	if (footer_id == 2) {
