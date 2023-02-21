c1f8da7eccd4fe27a7eaf35360a20028fea7eb04
AnkitYanis <8746855+CiaoAnkit@users.noreply.github.com>
2023-02-21 15:02:16 +0530

    C15

--- js/commonJSFeatures.js
+++ js/commonJSFeatures.js
@@ -79,7 +79,7 @@ document.getElementById("header_area_id").innerHTML +=  "\
 	var curr_year = new Date();
 
 	if (footer_id == 1) {
-		update_date = "December 21, 2022";
+		update_date = "December 22, 2022";
 	}
 
 	if (footer_id == 2) {
