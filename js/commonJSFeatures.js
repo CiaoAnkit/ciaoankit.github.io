dc108ecd61473396c82b359bb8920f8769be07dd
AnkitXerxes <8746855+CiaoAnkit@users.noreply.github.com>
2025-07-06 14:51:09 +0530

    C22_C23

--- js/commonJSFeatures.js
+++ js/commonJSFeatures.js
@@ -77,7 +77,7 @@ document.getElementById("header_area_id").innerHTML +=  "\
 	const today = new Date();
 
 	if (footer_id == 1) {
-		update_date = "May 27, 2025";
+		update_date = "July 05, 2025";
 	}
 
 	if (footer_id == 2) {
